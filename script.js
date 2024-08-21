32d35ca8903700893a4c91bf0004238a67728634
HARI PRASATH <147850207+hari7702@users.noreply.github.com>
2024-08-21 22:29:23 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,43 @@
+const texts = [
+    "Aspiring Data Scientist",
+    "AI Enthusiast",
+    "A Tech Explorer",
+    "A Graphic Designer",
+    "Karate Mentor"
+];
+
+let count = 0;
+let index = 0;
+let currentText = '';
+let letter = '';
+
+(function type() {
+    if (count === texts.length) {
+        count = 0;
+    }
+    currentText = texts[count];
+    letter = currentText.slice(0, ++index);
+
+    document.getElementById('typewriter-text').textContent = letter;
+
+    if (letter.length === currentText.length) {
+        count++;
+        index = 0;
+        setTimeout(type, 2000); // Adjust delay before the next text appears
+    } else {
+        setTimeout(type, 200); // Adjust typing speed for better visibility
+    }
+}());
+
+
+
 $(document).ready(function(){
     $('.project-slider').slick({
         autoplay: true,
         autoplaySpeed: 2000,
         fade: true,
         speed: 700,
-        dots: true,
+        dots: false,  // Disabled dots
         arrows: true
     });
 
@@ -21,46 +54,9 @@ $(document).ready(function(){
         $('.res-navbar').removeClass('show');
     });
 
-    const texts = [
-        "Aspiring Data Scientist",
-        "AI Enthusiast",
-        "A Tech Explorer",
-        "A Graphic Designer",
-        "Karate Mentor"
-    ];
-    
-    let count = 0;
-    let index = 0;
-    let currentText = '';
-    let letter = '';
-    
-    (function type() {
-        if (count === texts.length) {
-            count = 0;
-        }
-        currentText = texts[count];
-        letter = currentText.slice(0, ++index);
-    
-        document.getElementById('typewriter-text').textContent = letter;
-    
-        if (letter.length === currentText.length) {
-            count++;
-            index = 0;
-            setTimeout(type, 1000); // Wait 1 second before typing the next word
-        } else {
-            setTimeout(type, 150); // Adjust the speed of typing
-        }
-    }());    
-
-    // Visitor Counter
-$(document).ready(function() {
-    var counter = 0;
-    if(localStorage.getItem('visitorCount')) {
-        counter = parseInt(localStorage.getItem('visitorCount'));
-    }
-    counter++;
-    localStorage.setItem('visitorCount', counter);
-    $('#visitor-counter').text(counter);
+    AOS.init({
+        duration: 1200,
+    });
 });
 
 function sendEmail(event) {
@@ -72,10 +68,3 @@ function sendEmail(event) {
     const mailtoLink = `mailto:harianirudh0@gmail.com?subject=Message from ${name}&body=Name: ${name}%0AEmail: ${email}%0AMessage: ${message}`;
     window.location.href = mailtoLink;
 }
-
-
-    AOS.init({
-        duration: 1200,
-    });
-    
-});
